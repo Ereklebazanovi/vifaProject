23cba9ce2a098869fcab2774626e4313adc5e400
Erekle Bazanovi <erekle.bazanovi.1@iliauni.edu.ge>
2025-09-21 21:01:19 +0400

    add ai chatbot best or fb page

--- src/pages/NewHome.tsx
+++ src/pages/NewHome.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import type React from "react";
-import { useState } from "react";
+// import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useLanguage } from "../contexts/LanguageContext";
 import { useLanguageTransition } from "../hooks/useLanguageTransition";
