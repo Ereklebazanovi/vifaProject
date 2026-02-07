3e2d13df5a25084f899b638da0353fed5e87c07f
Ereklebazanovi <erekle.bazanovi.1@iliauni.edu.ge>
2026-02-07 17:28:26 +0400

    Fix: Strict React 18, Framer 11, Fiber 8 + Diagnostics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+
+// 👇 დიაგნოსტიკა: ეს გამოაჩენს კონსოლში ზუსტ ვერსიას
+console.log(
+  "%c [DIAGNOSTIC] Running React Version: " + React.version, 
+  "background: #bada55; color: #222; font-size: 16px; padding: 4px; border-radius: 4px;"
+);
+
 // Defer non-critical optimizations to avoid blocking
 setTimeout(() => {
   import('./utils/mobileOptimizations').then(({ injectCriticalCSS, preloadCriticalResources, getDeviceInfo }) => {
